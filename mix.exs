de10653abe965f9d7fe091c47c18b60e00d2088f
memmughal <mem.mughal+github1@gmail.com>
2019-01-06 14:07:00 +0100

    Start application

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule TwitterMentions.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :extwitter]
+      extra_applications: [:logger, :extwitter],
+      mod: {TwitterMentions.Application, []}
     ]
   end
 
