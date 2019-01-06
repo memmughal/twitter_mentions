e8e4b11f09411da4b064fc0c57a8ad02788400f8
memmughal <mem.mughal+github1@gmail.com>
2019-01-06 15:36:59 +0100

    Remove unused code from schema file

--- lib/schemas/mentions.ex
+++ lib/schemas/mentions.ex
@@ -1,16 +1,6 @@
 defmodule TwitterMentions.Schemas.Mentions do
   use Ecto.Schema
 
-  import Ecto.Changeset
-
-  @required_field [
-    :mention_screen_name,
-    :author_name,
-    :text,
-    :tweet_creation_date,
-    :number_of_retweets
-  ]
-
   @primary_key {:id, :binary_id, autogenerate: true}
   schema "mentions" do
     field(:mention_screen_name, :string)
@@ -20,25 +10,5 @@ defmodule TwitterMentions.Schemas.Mentions do
     field(:number_of_retweets, :integer)
 
     timestamps()
-  end
-
-  def changeset(%{} = params) do
-    %__MODULE__{}
-    |> cast(params, @required_field)
-    |> validate_required(@required_field)
-  end
-
-  def insert(
-        %{
-          mention_screen_name: _,
-          author_name: _,
-          text: _,
-          tweet_creation_date: _,
-          number_of_retweets: _
-        } = params
-      ) do
-    params
-    |> changeset()
-    |> TwitterMentions.Repo.insert()
   end
 end
