a2a068f555824817e1958612b54a6a9bcf985e2c
memmughal <mem.mughal+github1@gmail.com>
2019-01-06 16:52:02 +0100

    Refactor to take twitter related functionality into a separate module

---
+++ lib/twitter_client/twitter.ex
@@ -0,0 +1,44 @@
+defmodule TwitterMentions.TwitterClient.Twitter do
+  @moduledoc """
+  Module to call twitter api methods.
+  """
+  require Logger
+
+  def get_mentions!(screen_name: screen_name) do
+    try do
+      check_twitter_auth_credentials()
+
+      ExTwitter.search(screen_name, count: 2)
+    rescue
+      ExTwitter.RateLimitExceededError ->
+        Logger.error("Rate limit exceeded, please try again later")
+        {:error, :rate_limit_exceeded}
+
+      ExTwitter.Error ->
+        Logger.error("Please check twitter auth credentials in the config")
+        {:error, :twitter_error}
+
+      error ->
+        Logger.error("Some error occurred #{inspect(error)}")
+        error
+    end
+  end
+
+  defp check_twitter_auth_credentials() do
+    keys = [:consumer_key, :consumer_secret, :access_token, :access_token_secret]
+    Enum.each(keys, &get_twitter_config_value!/1)
+
+    :ok
+  end
+
+  defp get_twitter_config_value!(key) do
+    :extwitter
+    |> Application.fetch_env!(:oauth)
+    |> Keyword.fetch!(key)
+    |> raise_if_invalid!(key)
+  end
+
+  defp raise_if_invalid!(nil, key), do: raise("expects #{key} to be configured for extwitter")
+  defp raise_if_invalid!("", key), do: raise("expects #{key} to be configured for extwitter")
+  defp raise_if_invalid!(value, _key), do: value
+end
--- lib/twitter_mentions.ex
+++ lib/twitter_mentions.ex
@@ -31,29 +31,15 @@ defmodule TwitterMentions do
     it will respond with tweets before this date
   """
   def fetch do
-    try do
-      check_twitter_auth_credentials()
+    mentions =
+      [screen_name: get_screen_name()]
+      |> TwitterMentions.TwitterClient.Twitter.get_mentions!()
+      |> map_mentions()
 
-      mentions =
-        get_screen_name()
-        |> ExTwitter.search(count: 2)
-        |> map_mentions()
-
-      Repo.insert_all(Mentions, mentions, on_conflict: :nothing)
-    rescue
-      ExTwitter.RateLimitExceededError ->
-        Logger.error("Rate limit exceeded, please try again later")
-        {:error, :rate_limit_exceeded}
-
-      ExTwitter.Error ->
-        Logger.error("Please check twitter auth credentials in the config")
-        {:error, :twitter_error}
-
-      error ->
-        Logger.error("Some error occurred #{inspect(error)}")
-        error
-    end
+    Repo.insert_all(Mentions, mentions, on_conflict: :nothing)
   end
+
+  defp get_screen_name, do: Application.fetch_env!(:twitter_mentions, :screen_name)
 
   defp map_mentions([]), do: {:error, :no_tweets_found}
   defp map_mentions(mentions) when is_list(mentions), do: Enum.map(mentions, &format_mention/1)
@@ -70,26 +56,6 @@ defmodule TwitterMentions do
       updated_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
     }
   end
-
-  defp get_screen_name, do: Application.fetch_env!(:twitter_mentions, :screen_name)
-
-  defp check_twitter_auth_credentials() do
-    keys = [:consumer_key, :consumer_secret, :access_token, :access_token_secret]
-    Enum.each(keys, &get_twitter_config_value!/1)
-
-    :ok
-  end
-
-  defp get_twitter_config_value!(key) do
-    :extwitter
-    |> Application.fetch_env!(:oauth)
-    |> Keyword.get(key)
-    |> raise_if_invalid!(key)
-  end
-
-  defp raise_if_invalid!(nil, key), do: raise("expects #{key} to be configured for extwitter")
-  defp raise_if_invalid!("", key), do: raise("expects #{key} to be configured for extwitter")
-  defp raise_if_invalid!(value, _key), do: value
 
   def get_all, do: TwitterMentions.Repo.all(Mentions)
 end
